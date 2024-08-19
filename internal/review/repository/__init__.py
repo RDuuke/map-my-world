ded4c8ca76c3e2c2844e405a62bbe2cd3a7bed74
rduuke <juuanduuke@gmail.com>
2024-08-19 14:58:02 -0500

    fixed init

--- internal/review/repository/__init__.py
+++ internal/review/repository/__init__.py
@@ -1,5 +1,2 @@
 from internal.review.repository.repository import ReviewRepository  # noqa: F401
-<<<<<<< HEAD
-=======
 from internal.review.repository.repository_mongo import ReviewMongoRepository  # noqa :F401
->>>>>>> ac4b9b82da7931f423443fdae7262897168eed9f
--- src/api/handler/review/__init__.py
+++ src/api/handler/review/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-from src.api.handler.review.add_last_reviewed_handler import CreateUpdateLastReviewedHandler  # noqa: F401
-=======
 from src.api.handler.review.add_last_reviewed_handler import ReviewUpdateLastReviewedHandler  # noqa: F401
->>>>>>> ac4b9b82da7931f423443fdae7262897168eed9f
 from src.api.handler.review.create_handler import ReviewCreateHandler  # noqa :F401
 from src.api.handler.review.recommendation_handler import ReviewRecommendationHandler  # noqa: F401
